824f284d171dbbeba6cdc77eaca71f8f20f497e0
HasegawaShoichi <0ve38x163040p4z@ezweb.ne.jp>
2021-08-07 06:06:47 +0900

    Created code of reading reason data(txt data)

---
+++ src/global_knowledge.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ProbLogのライブラリ
+from problog.program import PrologString
+from problog import get_evaluatable
+from __init__ import *
+
+# 推論モデルの読み込み
+f = open(data_path, 'r')
+data = f.readlines()
+reasoning_data = '\n'.join(data)
+
+# 論理推論の実行
+p = PrologString(reasoning_data)
+
+# 推論結果の出力
+result = get_evaluatable().create_from(p).evaluate()
+print (result)
+max_value = max(result.values())
+print(max_value)
+max_key = str(max(result, key=result.get))
+print(max_key)
+key_start = max_key.find(',')
+key_goal = len(max_key)
+target_place = max_key[key_start+14 : key_goal-1]
+print(target_place)
+
+"""
+#対象の場所の語彙をtxtファイルへ書き出し
+path_w = "/root/HSR/catkin_ws/src/SpCoNavi/SIGVerse/planning/myfile.txt"
+f = open(path_w, 'w')
+f.write(target_place)
+f.close()
+
+#txtファイルの読み込み、変数へ代入
+with open(path_w) as f:
+  target = f.read()
+  print(target)
+
+#場所の語彙と対応する数字への置換
+list = {'entrance':0, 'living':1, 'dining':2, 'kitchen':3}
+target_instance = list[target]
+print(target_instance)
+"""
