5542edc0af5212627af937fca04ba3c0e4edb630
HasegawaShoichi <0ve38x163040p4z@ezweb.ne.jp>
2021-08-27 04:40:17 +0900

    Completed problog_ros

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,2 @@
-DATASET_FOLDER  = "../data/"
+#DATASET_FOLDER  = "../data/"
+DATASET_FOLDER = "/root/RULO/catkin_ws/src/problog_ros/data/"
--- src/global_knowledge.py
+++ src/global_knowledge.py
@@ -2,12 +2,26 @@
 # -*- coding: utf-8 -*-
 # 入力された物体名から存在しやすい場所の名前を論理推論するコード
 
-## 必要なライブラリ (Melodic環境 (Python2系環境)で動作させる場合)
+######## 必要なライブラリ (Melodic環境 (Python2系環境)で動作させる場合) ########
+
+## /root/RULO/catkin_ws/srcの中で
+# git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+# echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc	
+# echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+# echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+# source ~/.bashrc
+
+## /root/RULO/catkin_ws/srcの中で
+# git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+# echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+# source ~/.bashrc
+
 # apt-get install python3-pip
 # python3 -m pip install problog
 # python3 -m pip install pyyaml
 # python3 -m pip install rospkg catkin_pkg
 
+###############################################################################
 
 from problog.program import PrologString
 from problog import get_evaluatable
@@ -18,18 +32,16 @@ from __init__ import *
 class LogicalInference():
 
   def __init__(self):
-    rospy.Subscriber("/human_command", String, self.word_callback, queue_size=1)
+    #rospy.Subscriber("/human_command", String, self.word_callback, queue_size=1)
+    pass
 
-  def word_callback(self, word):
+
+  def word_callback(self):
+    word = rospy.wait_for_message("/human_command", String, timeout=None)
+
     # 推論モデルの読み込み
     object_name = word.data
-    #print(object_name)
-    #print(type(object_name))
-    #object_name = str(object_name)
-    #rospy.loginfo("object_name: ", object_name)
-    #object_name = "penguin_doll"
     TXT_DATA = DATASET_FOLDER + object_name + ".txt"
-    #print(TXT_DATA)
     f = open(TXT_DATA, 'r')
     reasoning_data = f.readlines()
     reasoning_data = '\n'.join(reasoning_data)
@@ -38,43 +50,30 @@ class LogicalInference():
     p = PrologString(reasoning_data)
 
     # 推論結果の出力
-    
     result = get_evaluatable().create_from(p).evaluate()
-    print (result)
-    pre_prob = list(result.values())
-    print(pre_prob)
+    #print (result)
+    place_names_probs = {}
+
+    # 推論した場所の単語と確率を辞書型に格納
+    pre_prob = list(result.values())  # 場所の確率
     
-    #return result
+    count = 0                         # 場所の単語 (場所の単語が最高で7文字なのでこのコードで動作可能)
+    for key in result.keys(): 
+      key_goal =len(str(key))
+      place_name = str(key)[key_goal-8 : key_goal-1]
 
-    """
-    max_value = max(result.values())
-    print(max_value)
-    max_key = str(max(result, key=result.get))
-    print(max_key)
-    key_start = max_key.find(',')
-    key_goal = len(max_key)
-    target_place = max_key[key_start+14 : key_goal-1]
-    print(target_place)
+      if place_name.find(',') is not None:
+        place_name = place_name[place_name.find(',') + 1 :]
 
+      place_names_probs[place_name] = pre_prob[count]
+      count += 1
+
+    print(place_names_probs)
+    return place_names_probs
     
-    #対象の場所の語彙をtxtファイルへ書き出し
-    path_w = "/root/HSR/catkin_ws/src/SpCoNavi/SIGVerse/planning/myfile.txt"
-    f = open(path_w, 'w')
-    f.write(target_place)
-    f.close()
-
-    #txtファイルの読み込み、変数へ代入
-    with open(path_w) as f:
-      target = f.read()
-      print(target)
-
-    #場所の語彙と対応する数字への置換
-    list = {'entrance':0, 'living':1, 'dining':2, 'kitchen':3}
-    target_instance = list[target]
-    print(target_instance)
-    """
 
 if __name__ == "__main__":
   rospy.init_node('problog_logical_inference')
-  LogicalInference()
+  l = LogicalInference()
+  l.word_callback()
   rospy.spin()
