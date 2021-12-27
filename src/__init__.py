e48e17fa5ff4082082f95ddb90b2be0f953cd857
Shoichi-Hasegawa0628 <hasegawa.shoichi@em.ci.ritsumei.ac.jp>
2021-12-27 18:26:15 +0900

    Completed program code

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,2 @@
 #DATASET_FOLDER  = "../data/"
-DATASET_FOLDER = "/root/RULO/catkin_ws/src/problog_ros/data/"
+DATASET_FOLDER = "/root/HSR/catkin_ws/src/problog_ros/data/"
--- src/problog_ros_output_prob.py
+++ src/problog_ros_output_prob.py
@@ -14,7 +14,7 @@ import rospy
 from std_msgs.msg import String
 from __init__ import *
 import csv
-DATASET_FOLDER = "/root/RULO/catkin_ws/src/problog_ros/data/"
+DATASET_FOLDER = "/root/HSR/catkin_ws/src/problog_ros/data/"
 
 class LogicalInference():
 
@@ -22,12 +22,12 @@ class LogicalInference():
     pass
 
 
-  def word_callback(self):
-    word = rospy.wait_for_message("/human_command", String, timeout=None)
+  def word_callback(self, target_name):
+    # word = rospy.wait_for_message("/human_command", String, timeout=None)
 
     # 推論モデルの読み込み
-    object_name = word.data
-    #object_name = "pig_doll"
+    # object_name = word.data
+    object_name = target_name
     TXT_DATA = DATASET_FOLDER + object_name + ".txt"
     f = open(TXT_DATA, 'r')
     reasoning_data = f.readlines()
@@ -38,15 +38,15 @@ class LogicalInference():
 
     # 推論結果の出力
     result = get_evaluatable().create_from(p).evaluate()
-    print("ProbLog result of reasoning\n")
-    print (result)
-    print("****************************************************************\n")
+    #print("ProbLog result of reasoning\n")
+    #print (result)
+    #print("****************************************************************\n")
 
     # 推論した場所の単語と確率を辞書型に格納
     pre_prob = list(result.values())  # 場所の確率
 
     # 場所の単語一覧をロード
-    with open('../data/3LDK_01_w_index_1_0.csv', 'r') as f:
+    with open('/root/HSR/catkin_ws/src/problog_ros/data/W_list.csv', 'r') as f:
       reader = csv.reader(f)
       for row in reader:
         pass
@@ -54,9 +54,20 @@ class LogicalInference():
 
     place_name_probs = [0, 0, 0, 0]
     count = 0                         # 場所の単語 (場所の単語が最高で7文字なのでこのコードで動作可能)
-    for key in result.keys(): 
+    ct = 1
+    for key in result.keys():
       key_goal =len(str(key))
-      place_name = str(key)[key_goal-8 : key_goal-1]
+      #place_name = str(key)[key_goal - 7: key_goal - 1]
+      if ct == 1:
+        place_name = str(key)[key_goal-7 : key_goal-1]
+      elif ct == 2:
+        place_name = str(key)[key_goal - 8: key_goal - 1]
+      elif ct == 3:
+        place_name = str(key)[key_goal - 8: key_goal - 1]
+      else:
+        place_name = str(key)[key_goal - 9: key_goal - 1]
+      # print(place_name)
+      # ct += 1
 
       if place_name.find(',') is not None:
         place_name = place_name[place_name.find(',') + 1 :]
@@ -64,10 +75,12 @@ class LogicalInference():
       j = place_name_list.index(place_name)
       place_name_probs[j] = pre_prob[count]
       count += 1
+      ct += 1
 
     #print(place_name_probs)
     place_name_probs = [float(i)/sum(place_name_probs) for i in place_name_probs]  #正規化
-    #print(sum(place_name_probs))
+    # print(sum(place_name_probs))
+    # print(place_name_probs)
     return place_name_probs
 
 
